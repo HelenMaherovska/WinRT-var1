62e4ad74e70d211fba927248b613d5e962bb7c7f
Helen <maher.olena96@gmail.com>
2017-06-21 00:03:49 +0300

    first commit

---
+++ Professorweb_6_2/Professorweb_6_2.Windows/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Professorweb_6_2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+            if (help.Info != null)
+            {
+                txt.Text = help.Info.ToString();
+            }
+        }
+        protected override void OnNavigatedTo(Windows.UI.Xaml.Navigation.NavigationEventArgs e)
+        {
+            forwardBtn.IsEnabled = this.Frame.CanGoForward;
+            backBtn.IsEnabled = this.Frame.CanGoBack;
+            
+        }
+        private void SecondPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(SecondPage));
+        }
+        private void ForwardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.GoForward();
+        }
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.GoBack();
+        }
+        private void txt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Professorweb_6_2/Professorweb_6_2.Windows/help.cs
@@ -0,0 +1,7 @@
+﻿namespace Professorweb_6_2
+{
+    internal class help
+    {
+        public static string Info { get; internal set; }
+    }
+}
